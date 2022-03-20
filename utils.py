aeeb3349be97efe7677bbf2d78786d800868d4df
Kevin Chen <potatokevin123@gmail.com>
2022-03-20 14:07:49 -0400

    Fix stdout printing bug
    When buffer is not flushed, monitor printing buffers until program exit

--- utils.py
+++ utils.py
@@ -12,19 +12,19 @@ class Logger:
     def alert(self, msg, tag="ALERT"):
         current_time = datetime.now().strftime("%I:%M:%S.%f %p")
         text = "[{}] {} [{}] -> {}".format(current_time, self.identifier, tag, msg)
-        print(stylize(text, colored.fg("magenta")))
+        print(stylize(text, colored.fg("magenta")), flush=True)
 
     def info(self, msg, tag="INFO"):
         current_time = datetime.now().strftime("%I:%M:%S.%f %p")
         text = "[{}] {} [{}] -> {}".format(current_time, self.identifier, tag, msg)
-        print(stylize(text, colored.fg("cyan")))
+        print(stylize(text, colored.fg("cyan")), flush=True)
 
     def error(self, msg, tag="ERROR"):
         current_time = datetime.now().strftime("%I:%M:%S.%f %p")
         text = "[{}] {} [{}] -> {}".format(current_time, self.identifier, tag, msg)
-        print(stylize(text, colored.fg("red")))
+        print(stylize(text, colored.fg("red")), flush=True)
 
     def success(self, msg ,tag="SUCCESS"):
         current_time = datetime.now().strftime("%I:%M:%S.%f %p")
         text = "[{}] {} [{}] -> {}".format(current_time, self.identifier, tag, msg)
-        print(stylize(text, colored.fg("green")))
+        print(stylize(text, colored.fg("green")), flush=True)
